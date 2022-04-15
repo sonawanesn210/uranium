3c575ef09f99f6eb8934cfb6cf23255a5875a1ce
Your Name <you@example.com>
2022-04-15 18:02:15 +0530

    new

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
+/*router.get('/test-me', function (req, res) {
     // let a = { msg: "My first ever API response in JSON !!"} 
 
 
@@ -64,6 +64,71 @@ router.post('/test-post2', function (req, res) {
 const randomController= require("../controllers/randomController.js")
 //write a post request to accept an element in post request body and add it to the given array and return the new array
 router.post('/test-post3', randomController.addToArray ); //HANDLER/CONTROLLER
+
+*/
+router.get('/movies', function(req,res){
+    let arr =["Rang de Basanti","The Shining","Incendies","Finding Nemo","Lord of the Rings"]
+    console.log("Name of the movies")
+    res.send(arr)
+})
+
+router.get('/movies/:indexNumber', function(req,res){
+    const arr1=["Rang de Basanti","The Shining","Incendies","Finding Nemo","Lord of the Rings"]
+ const indexnum=req.params.indexNumber
+   //res.send(arr1[indexnum]) 
+  if(indexnum<arr1.length){
+       res.send(arr1[indexnum])
+   }else{
+       res.send("Use valid index")
+   }
+   
+})
+
+router.get('/films', function(req,res){
+    const arr3 =[
+        {
+        "id":1,
+        "name":"The Shining"
+    } ,
+    { "id":2,
+    "name":"Incendies"
+   },
+   { "id":3,
+    "name":"Incendies"
+   },
+   { "id":4,
+    "name":"Incendies"
+   
+    }]
+    res.send(arr3)
+});
+
+router.get('/films/:filmId', function(req,res){
+    const arr4 =[
+        {
+        "id":1,
+        "name":"The Shining"
+    } ,
+    { "id":2,
+    "name":"Incendies"
+   },
+   { "id":3,
+    "name":"Incendies"
+   },
+   { "id":4,
+    "name":"Incendies"
+   
+    }]
+    const filmid=req.params.filmId
+    if(filmid<arr4.length){
+    res.send(arr4[filmid])
+    }else{
+        res.send("No movie exists with this id")
+    }
+});
+
+
+
 
 
 
