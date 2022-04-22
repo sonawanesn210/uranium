b6b66aeffd3af28d053be88b9bf2256c753e43d4
Your Name <you@example.com>
2022-04-22 18:55:54 +0530

    post msg

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,4 +1,4 @@
-const authenticate = function(req, req, next) {
+/* const authenticate = function(req, req, next) {
     //check the token in request header
     //validate this token
 
@@ -9,4 +9,4 @@ const authenticate = function(req, req, next) {
 const authorise = function(req, res, next) {
     // comapre the logged in user's id and the id in request
     next()
-}
+} */
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController= require("../controllers/userController")
-
+//const mid=require("../middleware/auth")
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
@@ -15,6 +15,6 @@ router.get("/users/:userId", userController.getUserData)
 router.post("/users/:userId/posts", userController.postMessage)
 
 router.put("/users/:userId", userController.updateUser)
-router.delete('/users/:userId', userController.deleteUser)
+//router.delete('/users/:userId', userController.deleteUser)
 
 module.exports = router;
